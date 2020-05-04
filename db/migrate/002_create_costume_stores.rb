4c25a26dad8583e3f66de262a63e08c267ed6fff
Mitch Beebe <mitchell.beebe@flatironschool.com>
2020-05-04 18:23:34 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
 # Create your costume_stores migration here
+class CostumeStore < ActiveRecord::Migration[4.2]
+    def change
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location
+      t.integer :costume_inventory 
+      t.string :employee_count
+      t.boolean :
+      t.timestamps
+    end
+  end
+end
