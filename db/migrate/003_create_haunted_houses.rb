8bd33adfe75aa358814c21a8c4bce1eceb6154ea
Mitch Beebe <mitchell.beebe@flatironschool.com>
2020-05-04 18:40:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,4 +3,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
       t.string :name 
-      t.
+      t.string :location
+      t.string :theme 
+      t.float :price
+      t.boolean :family_friendly 
+      t.datetime :opening_time
+      t.datetime :closing_time
